98c9773ca26dbe0c9d7aa5a02e555ebb57a8094c
Arnaud Barotteaux <arnaudbarotteaux@gmail.com>
2026-02-03 21:19:06 +0100

    Fix: Remove unused android-library and kotlin-jvm plugins

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -11,7 +11,6 @@ buildscript {
 
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     id("com.facebook.react.rootproject")
 }
